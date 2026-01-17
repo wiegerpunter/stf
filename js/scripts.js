aa9e8de1a80f5300453cad5ecf98f32ebf620ce4
wpunter <w.r.punter@student.tue.nl>
2026-01-17 11:01:19 +0100

    rsvp, calendar, menu verandering

--- js/scripts.js
+++ js/scripts.js
@@ -184,23 +184,26 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Meike & Wieger's bruiloft",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('April 25, 2026 14:00'),
 
             // Event duration (IN MINUTES)
-            // duration: 120,
+            // duration: 660,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Apr 26, 2026 01:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'De Hut van Mie Pils, Waalre',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "We kunnen niet wachten je te verwelkomen. Voor vragen, contacteer de ceremoniemeesters Margot of Mieke via +31 6 12345678.",
+            
+            // Event Location
+            URL: 'https://maps.app.goo.gl/iSoiyddQaawVXN6j9'
         }
     });
 
@@ -215,7 +218,7 @@ $(document).ready(function () {
         $('#alert-wrapper').html(alert_markup('info', '<strong>Even geduld!</strong> Je gegevens worden opgeslagen.'));
 
         if (MD5($('#invite_code').val()) !== '815d73b64a7c90c5de1b8d7a8a79ba0e') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> De invite code is incorrect.'));
+            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> De invite code is niet correct.'));
         } else {
             $.post('https://script.google.com/macros/s/AKfycbzS5YN-EYHXNm2fRbATaFJ2JbmpP4hi28aN5NwDuLtvcY_0Qgtwy6A_wI5VvrnnIfPO0g/exec', data)
                 .done(function (data) {
